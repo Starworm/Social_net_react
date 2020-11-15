efe074b4246b18a683e304fd299e297c963a3101
Starworm <starworm82@gmail.com>
2020-11-15 22:31:31 +0300

    23

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+
+    return (
+        <header className='header'>
+            <img src="https://img2.freepng.ru/20180315/vrq/kisspng-logo-lion-shutterstock-red-gradient-lionhead-5aab05d93512d0.9761940315211575932174.jpg" />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <nav className='nav'>
+            <div className="chapt-group">
+                <div className="linkback"><a href="#">Profile</a></div>
+                <div className="linkback"><a href="#">Messages</a></div>
+                <div className="linkback"><a href="#">News</a></div>
+                <div className="linkback"><a href="#">Music</a></div>
+                <div className="linkback"><a href="#">Settings</a></div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
