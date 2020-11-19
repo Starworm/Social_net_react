e112507f60f5963b8a995a0a6447d104e233ad86
Starworm <starworm82@gmail.com>
2020-11-19 09:59:50 +0300

    27

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 
-const App = () => {
+let smth = () => <Dialogs />
+
+const App = (props) => {
   const text = 'IT-kamasutra.com ';
 
   return (
@@ -18,8 +20,10 @@ const App = () => {
         <Header />
         <Navbar />
         <div className='app-wrapper-content'>
-          <Route path='/dialogs' component={Dialogs} />
-          <Route path='/profile' component={Profile} />
+          {/* <Route path='/dialogs' component={Dialogs} /> */}
+          {/* <Route path='/profile' component={Profile} /> */}
+          <Route path='/dialogs' component={() => <Dialogs />} />
+          <Route path='/profile' render={() => <Profile />} />
           <Route path='/news' component={News} />
           <Route path='/music' component={Music} />
           <Route path='/settings' component={Settings} />
