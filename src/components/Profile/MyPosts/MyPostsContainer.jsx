64aadb46175dc8bdc753a529be31c4191a343747
Starworm <starworm82@gmail.com>
2020-11-29 16:49:12 +0300

    45-1

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -54,6 +54,6 @@ let mapDispatchToProps = (dispatch) => {
     }
 }
 
-// const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts);
+const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts);
 
 export default MyPostsContainer;
